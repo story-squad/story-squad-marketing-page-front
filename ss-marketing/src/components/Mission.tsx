62c8c250968b7ded8125829b16f62105d69cb5a2
Rob Lark <62610548+Robert-Lark@users.noreply.github.com>
2021-07-16 19:11:57 -0700

    dropdown in progress

---
+++ ss-marketing/src/components/Mission.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Mission() {
+    return (
+        <div>
+            
+        </div>
+    );
+}
+
+export default Mission;
---
+++ ss-marketing/src/components/common/Header.tsx
@@ -0,0 +1,44 @@
+import styled from "styled-components";
+import Dropdown from './Dropdown'
+//styles
+
+function Header() {
+  return (
+    <Nav>
+      <Logo>
+        <p>LOGO</p>
+      </Logo>
+      <NavItems>
+        <a>Mission</a>
+        <Products>
+        <Dropdown/>
+        </Products>
+        <a>About Us</a>
+        <a>Contact</a>
+        <button>Sign Up</button>
+      </NavItems>
+    </Nav>
+  );
+}
+
+const Nav = styled.div`
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  min-height: 8vh;
+  background-color: #0089ed;
+`;
+
+const Logo = styled.div``;
+
+const NavItems = styled.ul`
+  display: flex;
+  justify-content: space-around;
+  width: 35%;
+`;
+const Products = styled.div `
+border: 2px solid black;
+overflow: visible;
+`
+
+export default Header;
