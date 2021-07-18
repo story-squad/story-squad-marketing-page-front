5e94b80f2bf1f6ae66e7888d10676598167bf714
Rob Lark <62610548+Robert-Lark@users.noreply.github.com>
2021-07-18 16:50:08 -0700

    completed mission

--- ss-marketing/src/components/Home/Mission.tsx
+++ ss-marketing/src/components/Home/Mission.tsx
@@ -1,81 +1,77 @@
 import "./styles/Mission.css";
-import Brain from "./imgs/emoji-brain.png"
-import Art from "./imgs/emoji-art.png"
-import Trophy from "./imgs/emoji-trophy.png"
-import test from './imgs/mission.png'
-
+import MissionImg from "./imgs/mission.png";
+import DemographicImg from "./imgs/demographic.png";
+import DiffenceImg from "./imgs/different.png";
+import Brain from "./imgs/emoji-brain.png";
+import Art from "./imgs/emoji-art.png";
+import Trophy from "./imgs/emoji-trophy.png";
 function Mission() {
   return (
-    <div className="missionContainer">
-        <div className='container_row'>
-        <img className="layer1" src={test} alt="test" />
-      <div className="mission layer2">
-        <h2>Our Mission</h2>
-        <p>Human connection through creative expression.</p>
-      </div>
-      </div>
-      <div className="demographic">
-        <h2>Who's it for?</h2>
-        <p>8-12 year olds & adults young at heart!</p>
-      </div>
-      <div className="different">
-        <h2>How are we different?</h2>
-        <p>At Story Squad we are creating something unique</p>
-        <div className="innerMissionContainerDesktop">
-          <div className="differentContent">
-            <img src={Brain} alt="brain" />
-            <p>
-              For fun, kids—whose brains are vulnerable to blasts of
-              dopamine—are bingeing on Fortnite, TikTok or YouTube & these
-              rings, dings & pings keep kids staring zombie-fied in a digital
-              abyss.
-            </p>
-          </div>
-          <div className="differentContent">
-            <img src={Art} alt="pallete" />
-            <p>
-              Story Squad offers a radical alternative: maximize the time your
-              child spends offline in “creative mode” armed with only loose-leaf
-              sheets of paper and a pencil.
-            </p>
-          </div>
-          <div className="differentContent">
-            <img src={Trophy} alt="trophy" />
-            <p>
-              We re-purpose the reward structure of video games—an activity kids
-              know all too well—to motivate them in their reading, writing &
-              drawing.
-            </p>
+    <div className="MissionContainer">
+      <div className="missionBlock">
+        <div className="grid-div-mission">
+          <img className="missionLayer1 first" src={MissionImg} alt="first" />
+
+          <div className="missionLayer2">
+            <h1>Our Mission</h1>
+            <p>Human connection through creative expression.</p>
           </div>
         </div>
       </div>
-      <div className="innerMissionContainerMobile">
-          <div className="differentContent">
-            <img src={Brain} alt="brain" />
-            <p>
-              For fun, kids—whose brains are vulnerable to blasts of
-              dopamine—are bingeing on Fortnite, TikTok or YouTube & these
-              rings, dings & pings keep kids staring zombie-fied in a digital
-              abyss.
-            </p>
-          </div>
-          <div className="differentContent">
-            <img src={Art} alt="pallete" />
-            <p>
-              Story Squad offers a radical alternative: maximize the time your
-              child spends offline in “creative mode” armed with only loose-leaf
-              sheets of paper and a pencil.
-            </p>
-          </div>
-          <div className="differentContent">
-            <img src={Trophy} alt="trophy" />
-            <p>
-              We re-purpose the reward structure of video games—an activity kids
-              know all too well—to motivate them in their reading, writing &
-              drawing.
-            </p>
+
+      <div className="missionBlock">
+        <div className="grid-div-mission">
+          <img
+            className="missionLayer1 second"
+            src={DemographicImg}
+            alt="first"
+          />
+
+          <div className="missionLayer2">
+            <h1>WHO’S IT FOR?</h1>
+            <p>8-12 year olds & adults young at heart!</p>
           </div>
         </div>
+      </div>
+
+      <div className="missionBlock">
+        <div className="grid-div-mission">
+          <img className="missionLayer1 third" src={DiffenceImg} alt="first" />
+
+          <div className="missionLayer2">
+            <h1>HOW ARE WE DIFFERENT?</h1>
+            <p className="missionMobile">Achieving unique things everyday</p>
+
+            <div className="missionDesktop">
+              <div className="differentOne">
+                <img src={Brain} alt="brain" />
+                <p>
+                  For fun, kids—whose brains are vulnerable to blasts of
+                  dopamine—are bingeing on Fortnite, TikTok or YouTube & these
+                  rings, dings & pings keep kids staring zombie-fied in a
+                  digital abyss.
+                </p>
+              </div>
+              <div className="differentTwo">
+                <img src={Art} alt="Art" />
+                <p>
+                  Story Squad offers a radical alternative: maximize the time
+                  your child spends offline in “creative mode” armed with only
+                  loose-leaf sheets of paper and a pencil.
+                </p>
+              </div>
+              <div className="differentThree">
+                <img src={Trophy} alt="Trophy" />
+                <p>
+                  We re-purpose the reward structure of video games—an activity
+                  kids know all too well—to motivate them in their reading,
+                  writing & drawing.
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
