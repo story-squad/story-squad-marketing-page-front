4acc18f2e8172fbc358fd4b31588512a0ba9a861
Rob Lark <62610548+Robert-Lark@users.noreply.github.com>
2021-07-18 11:12:06 -0700

    added mission page

--- ss-marketing/src/components/Home/Mission.tsx
+++ ss-marketing/src/components/Home/Mission.tsx
@@ -1,17 +1,46 @@
-import React from 'react';
-import styled from "styled-components";
-
-const Container = styled.div`
-
-`
-
-
 function Mission() {
-    return (
-        <Container>
-            
-        </Container>
-    );
+  return (
+    <div className="missionContainer">
+      <div className="mission">
+        <h2>Our Mission</h2>
+        <p></p>
+      </div>
+      <div className="demographic">
+        <h2>Who's it for?</h2>
+        <p></p>
+      </div>
+      <div className="different">
+        <h2>How are we different?</h2>
+        <div className='innerMissionContainer'>
+          <div>
+            <img src="" alt="brain" />
+            <p>
+              For fun, kids—whose brains are vulnerable to blasts of
+              dopamine—are bingeing on Fortnite, TikTok or YouTube & these
+              rings, dings & pings keep kids staring zombie-fied in a digital
+              abyss.
+            </p>
+          </div>
+          <div>
+            <img src="" alt="pallete" />
+            <p>
+              Story Squad offers a radical alternative: maximize the time your
+              child spends offline in “creative mode” armed with only loose-leaf
+              sheets of paper and a pencil.
+            </p>
+          </div>
+          <div>
+            <img src="" alt="trophy" />
+            <p>
+              We re-purpose the reward structure of video games—an activity kids
+              know all too well—to motivate them in their reading, writing &
+              drawing.
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Mission;
