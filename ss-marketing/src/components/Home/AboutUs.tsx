ad9bda97041a6a43c85ad8a212f1bdb18baf5bdb
Rob Lark <62610548+Robert-Lark@users.noreply.github.com>
2021-07-19 11:51:30 -0700

    about us mobile complete

--- ss-marketing/src/components/Home/AboutUs.tsx
+++ ss-marketing/src/components/Home/AboutUs.tsx
@@ -1,17 +1,60 @@
-import React from 'react';
-import styled from "styled-components";
-
-const Container = styled.div`
-
-`
-
+import "./styles/AboutUs.css";
+import Graig from "./imgs/Graig.png";
+import Darwin from "./imgs/Darwin.png";
 
 function AboutUs() {
-    return (
-        <Container>
-            
-        </Container>
-    );
+  return (
+    <div>
+      <div className="aboutUsTop">
+        <h1>ABOUT US</h1>
+        <p>
+          “The imagination is central to life and to learning at every age and
+          that it is through the exercise of the imagination, grounded in play,
+          that children begin to explore the human condition.”
+        </p>
+        <p>—Professor Michael Armstrong, Bread Loaf School of English</p>
+        <p>
+          Kids know how to tell sophisticated and clever and silly and scary and
+          whimsical and fantastic and adventurous and heartbreaking stories at a
+          very young age.
+        </p>
+        <p>
+          Preserving access to this literary imagination is a project worth
+          championing for everyone, and it is why Story Squad was founded in the
+          first place.
+        </p>
+      </div>
+      <div className="aboutUsBottom">
+        <div className="founder">
+          <img className='graig' src={Graig} alt="Founder 1" />
+          <div className="founderTextContent">
+            <h1>Graig Peterson, co-founder</h1>
+            <p>
+              Graig is a former classroom teacher, management consultant and
+              PADI Divemaster with an AB from Dartmouth and an MA in English
+              from Middlebury. His favorite book series in 4th grade was the
+              Boxcar Children.
+            </p>
+          </div>
+        </div>
+        <div className="founder">
+          <img className='darwin' src={Darwin} alt="Founder 2" />
+          <div className="founderTextContent">
+            <h1>Darwin Johnson, co-founder</h1>
+            <p>
+              Darwin is a Graduate of Lambda School’s inaugural Data Science
+              Cohort and was Section Lead for its 3rd DS Cohort. He has over 20
+              years’ experience in the IT consulting space, including three
+              years as IT Director for the Somaly Mam Foundation, an
+              international non-profit fighting modern day slavery. His career
+              focus has been at the intersection of technology and social
+              responsibility.
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default AboutUs;
